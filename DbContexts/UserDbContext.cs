968efe6743628ab715eb5eff68d6618cad5836a6
Gerald Mathabela <Gerald.Mathabela@investec.co.uk>
2022-05-15 16:53:21 +0200

    Initial

---
+++ DbContexts/UserDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuthenticationService.Entities;
+
+namespace AuthenticationService.DbContexts
+{
+    public class UserDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public UserDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
+
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using AuthenticationService.Requests;
+using AuthenticationService.Entities;
+
+namespace AuthenticationService.Repositories
+{
+    public interface IUserRepository
+    {
+        public dynamic Authenticate(AuthenticateRequest authenticateRequest);
+        public IEnumerable<UserEntity> GetAll();
+        public UserEntity GetById(int id);
+        public dynamic CreateAccount(UserEntity userEntity);
+        public dynamic ActivateAccount(int userID, string username);
+    }
+}
+
